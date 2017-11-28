5ba65ba6844d6dd54eda793d9229097d4df284ae
Lynn_P <838327553@qq.com>
2017-11-28 09:32:51 +0800

    socket.io

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('login', {});
+});
+
+router.get('/index', function(req, res, next) {
+    res.render('index', {});
+})
+
+router.get('/chat', function(req, res, next) {
+    res.render('chat', {});
+})
+
+module.exports = router;
